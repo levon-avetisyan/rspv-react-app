b3af9c22c6600fc8a8d46480063d5782491d6ac8
levon-avetisyan <levon.s.avetisyan@gmail.com>
2019-03-10 14:55:38 +0400

    Updates

---
+++ precache-manifest.870a6769b8865a969d3d086d61a74410.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be92ab00f0f555419adb491aa392b9ca",
+    "url": "/rspv-react-app/static/media/header-bg.be92ab00.jpg"
+  },
+  {
+    "revision": "b236c022db87ac5b3a21",
+    "url": "/rspv-react-app/static/js/runtime~main.82143934.js"
+  },
+  {
+    "revision": "f21750104870905a1a6a",
+    "url": "/rspv-react-app/static/js/main.455d8918.chunk.js"
+  },
+  {
+    "revision": "685809411e8853ef0fc6",
+    "url": "/rspv-react-app/static/js/2.c86740fb.chunk.js"
+  },
+  {
+    "revision": "f21750104870905a1a6a",
+    "url": "/rspv-react-app/static/css/main.258f69d0.chunk.css"
+  },
+  {
+    "revision": "0345e30c5304a9e8263b14d4a1d394cc",
+    "url": "/rspv-react-app/index.html"
+  }
+];
